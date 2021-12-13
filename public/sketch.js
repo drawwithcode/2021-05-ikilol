b1b64ed26affa4a28d7829e25b75f2c5b08aa496
ikilol <87668373+ikilol@users.noreply.github.com>
2021-12-13 17:46:20 +0100

    push 05

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,9 @@
 var socket;
+var thirdText;
 
 function setup() {
-  createCanvas(400, 400);
-  background(21);
+  createCanvas(windowWidth, windowHeight);
+  background("#F0C1BC");
 
   socket = io.connect('http://localhost:3000');
   socket.on('mouse', newDrawing)
@@ -10,7 +11,7 @@ function setup() {
 
 function newDrawing(data){
   noStroke();
-  fill(255, 0, 100);
+  fill("#A8271D");
   ellipse(data.x, data.y, 36, 36);
 }
 
@@ -26,10 +27,19 @@ function mouseDragged(){
   socket.emit('mouse', data)
 
   noStroke();
-  fill(255);
+  fill("#AF1B1B");
   ellipse(mouseX, mouseY, 60, 60);
 }
 
 function draw() {
+
+  var thirdText = "Connect with someone and show some love!";
+  
+    textFont("VT323");
+    textAlign(CENTER);
+    textSize(60);
+    fill(255);
+
+    text(thirdText, width/2, 100);
 
 }
