db7d002cec02fb3b0ba1120c46dfcda01f5a7b5f
ikilol <87668373+ikilol@users.noreply.github.com>
2021-12-13 16:52:25 +0100

    PUSH04

--- public/sketch.js
+++ public/sketch.js
@@ -2,12 +2,34 @@ var socket;
 
 function setup() {
   createCanvas(400, 400);
+  background(21);
 
   socket = io.connect('http://localhost:3000');
+  socket.on('mouse', newDrawing)
+}
+
+function newDrawing(data){
+  noStroke();
+  fill(255, 0, 100);
+  ellipse(data.x, data.y, 36, 36);
+}
+
+function mouseDragged(){
+  console.log(mouseX + ',' + mouseY);
+
+  var data = {
+
+    x: mouseX,
+    y: mouseY
+  }
+
+  socket.emit('mouse', data)
+
+  noStroke();
+  fill(255);
+  ellipse(mouseX, mouseY, 60, 60);
 }
 
 function draw() {
-  background(21);
-  fill(255);
-  ellipse(mouseX, mouseY, 60, 60);
+
 }
--- server.js
+++ server.js
@@ -14,5 +14,11 @@ var io = socket(server);
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket){
-    console.log('newConnection'+ socket.id);
+    console.log('newConnection '+ socket.id);
+
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data) {
+        console.log(data);
+    }
 }
