b6ca8b104612b30568289a7b50399e6a926bda62
ikilol <87668373+ikilol@users.noreply.github.com>
2022-01-31 18:25:11 +0100

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,18 @@
+let clientSocket = io();
+
+clientSocket.on("connect", newConnection);
+clientSocket.on("mouseBroadcast", newBroadcast);
+
+function newConnection() {
+  console.log(clientSocket.id);
+}
+
+function newBroadcast(data) {
+  fill("red");
+  noStroke();
+  circle(data.x, data.y, 20);
+}
+
 var socket;
 var thirdText;
 
