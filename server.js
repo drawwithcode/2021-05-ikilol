23b2052c8fd5cd54a1c09262835636fa70009eda
ikilol <87668373+ikilol@users.noreply.github.com>
2022-01-31 18:19:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
-console.log("running the server!");
-//  Load express
 let express = require("express");
-let app = express();
-//  Defined port (for localhost)
-//  Port created by heroku or Locally
-const port = process.env.PORT || 8000;
 
-//  Defined server and connection
+let app = express();
+
+let port = process.env.PORT || 3000;
+
 let server = app.listen(port);
-console.log("Server is running on https://localhost:" + port);
+
+//console.log("server is running on http://localhost:" + port);
 
 app.use(express.static("public"));
 
-app.use(express.static('public'));
+let serverSocket = require("socket.io");
 
-console.log("server running");
+let io = serverSocket(server);
 
-var socket = require('socket.io');
+io.on("connection", newConnection);
 
-var io = socket(server);
+function newConnection(newSocket) {
+  //console.log(newSocket.id);
 
-io.sockets.on('connection', newConnection);
+  newSocket.on("mouse", mouseMessage);
 
-function newConnection(socket){
-    console.log('newConnection '+ socket.id);
-
-    socket.on('mouse', mouseMsg);
-
-    function mouseMsg(data) {
-        socket.broadcast.emit('mouse', data)
-        console.log(data);
-    }
+  function mouseMessage(dataReceived) {
+    newSocket.broadcast.emit("mouseBroadcast", dataReceived);
+  }
 }
